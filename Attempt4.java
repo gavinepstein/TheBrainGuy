ae3b55bf11e158c7dcdc0cf5508c3f31ef755e7c
Gavin Epstein <gepstein@cs.oberlin.edu>
2018-12-01 18:32:58 -0500

    New synthesizer, plays left to right

---
+++ Attempt4.java
@@ -0,0 +1,350 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.*;
+import java.util.stream.Stream;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSlider;
+import javax.swing.ListSelectionModel;
+import javax.swing.border.CompoundBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.text.Keymap;
+
+import processing.core.PApplet;
+
+
+public class Attempt4 extends JFrame{
+
+
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static boolean clearNow = false;
+	private static boolean  isplaying = false;
+	// set up buttons for the top of the screen 
+	JButton saveButton = new JButton("Export"); 
+	JButton clearButton = new JButton("Clear");
+	JButton goToKeyboardButton = new JButton("Go to Keyboard");
+	JButton playButton = new JButton("  Play   ");
+	JButton pauseButton = new JButton("Pause");
+	//number of seconds in the playback
+	private static int timelength = 5;
+	//handles all the audio
+
+	SoundModule audioPlayer = new SoundModule();
+	// set up title text at the top of the screen
+	JLabel screenTop = new JLabel("Create Instrument Mode");
+
+	// set up title text for the lower areas
+	JLabel drawingToolsLabel = new JLabel("Drawing Tools");
+	JLabel adjusterLabel = new JLabel("Adjuster Tools");
+	JLabel instrumentSelectLabel = new JLabel("Instrument Select");
+
+	//panelz
+	JPanel mainPanel = new JPanel();
+	// create top panel for box 
+	JPanel topPanel = new JPanel();
+	// create the center panel to hold the image 
+	JPanel centerPanel = new JPanel();
+	// create the bottom panel to hold the box 
+	JPanel bottomPanel = new JPanel(); 
+
+	JPanel bottomLeft = new JPanel();
+	JPanel bottomCenter = new JPanel();
+	JPanel bottomRight = new JPanel();
+
+	// create default list & scroller for the instrument select scroller
+	DefaultListModel instrumentSelectList = new DefaultListModel();
+	JScrollPane instrumentScroller;
+	JList instrumentsList;
+
+	// create dimensions for reference
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+
+	public Attempt4() {
+		// set up the main window size, location, close type, title
+		this.setSize(1000, 600);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("AV: Instrument Mode");
+		//Button stuff
+		clearButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				clear();
+			}
+		});
+
+		saveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				exportSound();
+			}
+		});
+
+		/** 
+		 * Create three body panels: Main back panel, top panel for labels, mid label for 
+		 *  drawing screen, bottom label for adjustment screens 
+		 */
+
+		// create the first body panel
+		mainPanel.setLayout(new BorderLayout());
+		// create top panel for box 
+		topPanel.setBackground(new Color(230,230,250));
+
+		// temporarily set bottom panel color 
+		bottomPanel.setLayout(new BorderLayout());
+		bottomPanel.setBackground(new Color (184, 168, 221));
+
+
+		/** 
+		 * Create three bottom panels that have matte filled borders. 
+		 */
+
+		// create bottom left panel and set an etched border
+		bottomLeft.setBackground(new Color(230,230,250));
+		bottomLeft.setBorder( new CompoundBorder(
+				BorderFactory.createMatteBorder(10,10,10,10, new Color(75, 0, 130)),
+				BorderFactory.createEtchedBorder()));
+
+
+		// create bottom center panel and set an etched border
+		bottomCenter.setBackground(new Color(230,230,250));
+		bottomCenter.setBorder( new CompoundBorder(
+				BorderFactory.createMatteBorder(10,10,10,10, new Color(75, 0, 130)),
+				BorderFactory.createEtchedBorder()));
+
+		// create bottom right panel and set an etched border
+		bottomRight.setBackground(new Color(230,230,250));
+		bottomRight.setBorder( new CompoundBorder(
+				BorderFactory.createMatteBorder(10,10,10,10, new Color(75, 0, 130)),
+				BorderFactory.createEtchedBorder()));
+
+		/** LTButtons (top of screen button options)  
+		 * Section includes: Creating the box to hold the top buttons and labels  
+		 */
+
+		// create box to hold middle top descriptor
+		Box LTButtons = Box.createHorizontalBox();
+		// box that holds the labels for the drawing screen area
+		Box box2 = Box.createHorizontalBox();
+
+		/** Left-hand top buttons (save, clear)
+		 * Section includes: Creating spacing between left-hand top buttons; adding to the box  
+		 */
+
+		// add save button to the top box
+		LTButtons.add(Box.createHorizontalGlue());
+		LTButtons.add(Box.createHorizontalStrut(4));
+		LTButtons.add(saveButton);
+		LTButtons.add(Box.createHorizontalStrut(4));
+
+		// add clear button to the top box
+		LTButtons.add(clearButton);
+		LTButtons.add(Box.createHorizontalStrut(280));
+
+
+		/** Create Instrument Mode  
+		 * Section includes: Adding the instrument mode label to the top bar's box 
+		 */
+
+
+		// add screen top label "Create Instrument Mode"
+		LTButtons.add(screenTop);
+		LTButtons.add(Box.createHorizontalStrut(100));
+
+		/** Right-hand top buttons (play, pause, go to keyboard)
+		 * Section includes: Creating spacing between the right-hand top buttons
+		 */
+
+
+
+
+		LTButtons.add(playButton);
+		LTButtons.add(Box.createHorizontalStrut(4));
+
+
+		LTButtons.add(pauseButton);
+		LTButtons.add(Box.createHorizontalStrut(4));
+
+		LTButtons.add(Box.createHorizontalGlue());		
+		LTButtons.add(Box.createHorizontalStrut(4));
+		LTButtons.add(goToKeyboardButton);
+
+
+
+		/** Bottom panel label bar  
+		 * Section includes: Adding drawing tools, adjuster tools, and instrument select labels to the purple mid-bar
+		 */
+
+		// add all of the bottom labels to box2 and place box2 at the north of bottomPanel
+		box2.add(Box.createHorizontalStrut((int)screenSize.getWidth()/70));
+		box2.add(drawingToolsLabel);
+		box2.add(Box.createHorizontalStrut(300));
+		box2.add(adjusterLabel);
+		box2.add(Box.createHorizontalGlue());
+		box2.add(instrumentSelectLabel);
+		box2.add(Box.createHorizontalStrut((int)screenSize.getWidth()/70));
+
+		/** Instrument list 
+		 * Section includes: Creating a list to hold all of the instruments, which will be displayed in the scroller 
+		 */
+
+		// create instrument scroller panel 
+		String[] instruments = {"Instrument 1", "Instrument 2", "Instrument 3", "Instrument 4", "Instrument 5"
+				, "Instrument 6", "Instrument 7"};
+		for(String instrument: instruments) {
+			instrumentSelectList.addElement(instrument);
+		}
+
+		/** Instrument Scroller and selection box: Stored in bottom right panel, East 
+		 * Section includes: Parameters for instrument spacing, row count, and scroll types 
+		 */
+
+		// create the list of instruments to go inside of the instrument scroller. set row count and width/height parameters.
+		instrumentsList = new JList(instrumentSelectList);
+		instrumentsList.setVisibleRowCount(4);
+		instrumentsList.setFixedCellHeight(60);
+		instrumentsList.setFixedCellWidth(150);
+		instrumentsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		// stick the list into the scrolling box 
+		instrumentScroller = new JScrollPane(instrumentsList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+
+		// add the scrolling box to the main panel, bottom right 
+		bottomRight.add(instrumentScroller, BorderLayout.EAST);
+
+		/** LTButtons (top of screen button options)  
+		 * Section includes: Adding the existing LTButtons box, which contains the buttons themselves, to the top 
+		 */
+
+		// add buttons to the very top of the screen 
+		topPanel.add(LTButtons);
+
+		/** Color Chooser 
+		 * Section includes: Adding the color chooser in to the bottom left of the screen 
+		 */
+
+		//add color chooser to bottom panel 
+		ColorChooser cc = new ColorChooser();
+		bottomLeft.add(cc);
+
+		/** TODO: Adjuster buttons (TEMPORARY PLACEMENT IN THE WRONG SECTION, UNTIL I'VE FIGURED OUT HOW TO ADD THEM TO THE 
+		 * DRAWING TOOLS BOX THAT ALREADY CONTAINS THE COLOR CHOOSER) 
+		 * Section includes: Adding adjuster buttons from separate class
+		 */
+		JButton squareToggle = new JButton("Square"); 
+		bottomCenter.add(squareToggle);
+		JButton lineToggle = new JButton("Line"); 
+		bottomCenter.add(lineToggle);
+
+		/** Processing display 
+		 * Section includes: Adding processing display from VisualStuff to the center panel for use
+		 */
+
+		//add processing display
+
+		JPanel display = VisualStuff.getDrawing(200, 200);
+		centerPanel.add(display);
+
+		//Play button
+		playButton.setMultiClickThreshhold(5400);
+
+		playButton.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+
+				JButton source= (JButton)e.getSource();
+				if (source.getModel().isPressed()){
+					if (!isplaying){
+						isplaying = true;
+						playButton.setText("Analyzing");
+
+						audioPlayer.interpretImage(VisualStuff.img, timelength);
+						playButton.setText(" Playing ");
+						audioPlayer.playSound();
+						playButton.setText("   Play  ");
+						System.out.println("play called");
+						//isplaying=false;
+					}
+				} else{
+					isplaying = false;
+				}
+			}
+
+
+		});
+
+
+		/** Bottom panel specifics & main panel additions 
+		 * Section includes: Adding the box of labels, the east/west/center panels on the bottom panel that contain 
+		 * adjusters and selectors, and adding the top, center, and bottom panels to the main panels 
+		 */
+
+
+		// add in all of the bottom panel specifics: box with labels and three panels
+		bottomPanel.add(box2, BorderLayout.NORTH);
+		bottomPanel.add(bottomRight, BorderLayout.EAST);
+		bottomPanel.add(bottomLeft, BorderLayout.WEST);
+		bottomPanel.add(bottomCenter, BorderLayout.CENTER);
+		mainPanel.add(topPanel, BorderLayout.NORTH);
+		mainPanel.add(centerPanel, BorderLayout.CENTER);
+		mainPanel.add(bottomPanel, BorderLayout.SOUTH);
+		this.add(mainPanel);
+
+		//topButtons.add(topButtonsNest, BorderLayout.NORTH);
+		this.setVisible(true); 
+	}
+
+	private void clear() {
+		VisualStuff.clearNow = true;
+	}
+
+	private void exportSound(){
+		JFileChooser fileChooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(".wav", "WAVE");
+		fileChooser.setFileFilter(filter);
+		if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+			File file = fileChooser.getSelectedFile();
+
+			AudioFileFormat.Type fileType = AudioFileFormat.Type.WAVE;
+			audioPlayer.saveFile(file);
+			;
+		}
+
+	}
+	private void saveImage(){
+		//TODO
+	}
+
+	public static void main(String[] args) {
+		new Attempt4();
+	}
+}
---
+++ SoundModule.java
@@ -0,0 +1,238 @@
+
+	import java.io.File;
+import java.time.Clock;
+import java.util.Random;
+import java.io.ByteArrayInputStream;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+
+	import javax.sound.sampled.AudioFormat;
+	import javax.sound.sampled.AudioFileFormat;
+	import javax.sound.sampled.AudioInputStream;
+	import javax.sound.sampled.AudioSystem;
+	import javax.sound.sampled.DataLine;
+	import javax.sound.sampled.SourceDataLine;
+
+
+public class SoundModule {
+	//sin table (for fast synthesis)
+	static float[]             sintable;
+	static float               sinstep;
+	static float               sininvStep;
+	static int                  sinsize;
+	private final static float tau   = (float)Math.PI * 2;
+	static
+	{
+	    sinsize = 10000;
+	    sintable = new float[sinsize];
+	    sinstep = tau / sinsize;
+	    sininvStep = 1.0f / sinstep;
+	    for (int i = 0; i < sinsize; ++i)
+	    {
+	        sintable[i] = (float) Math.sin(sinstep * i);
+	    }
+	}
+	
+	
+	private float sin(float a){
+		float t = a % tau;
+	    int indexA = (int) (t *sininvStep);
+	    int indexB = indexA + 1;
+	    if (indexB >= sinsize) return sintable[sinsize-1];
+	    float alpha = t *sininvStep - indexA;
+	    //linear interp.
+	    return (alpha) * sintable[indexA] + (1-alpha) *sintable[indexB];
+	}
+	
+	    private Sound currentsound;
+	    private static final int sliceWidth = 5;
+	    private static final int fftsize = 10;
+
+	    private static final int pixelsamplesize = 2;
+	    //Most of this code taken from https://introcs.cs.princeton.edu/java/15inout/Player.java 
+	    private class Sound{
+	    	private AudioFormat format;
+		    private int frames;         // total number of frames
+		    private int frameSize;      // bytes per frame (1 for 8-bit, 2 for 16-bit)
+		    private int bytes;          // total number of bytes
+		    private float frameRate;    // frames per second (44100 for CD audio)
+		    private double seconds;     // duration in seconds
+		    private byte[] data;
+	    
+	    // create new object from wav file
+	    	public Sound(String filename) {
+	        File file = new File(filename);
+	        try {
+	            AudioInputStream ais = AudioSystem.getAudioInputStream(file);
+	            AudioFileFormat  aff = AudioSystem.getAudioFileFormat(file);
+
+	            // get info
+	            format    = ais.getFormat();
+	            frames    = aff.getFrameLength();
+	            frameSize = format.getFrameSize();
+	            frameRate = format.getFrameRate();
+	            bytes     = frames * frameSize;
+	            seconds   = frames / frameRate;
+
+	            // read data
+	            data = new byte[bytes];
+	            ais.read(data);
+	        }
+	        catch (Exception e)  {
+	            System.out.println("File not Found");
+	            
+	        }
+	    }
+
+	    // create new object from byte array
+	    public Sound(byte[] data) {
+	        this.data = data.clone();
+	        // 44100Hz, 8 bit, 1 channel, PCM signed, little endian
+	        format = new AudioFormat(44100, 8, 1, true, false);
+	        frames    = data.length;
+	        frameSize = 1;   // 8-bit
+	        frameRate = 44100;
+	        bytes     = data.length;
+	        seconds   = frames / frameRate;
+	    }
+
+
+	    // play the sound on the sound card
+	    public void play() {
+	        SourceDataLine line = null;
+	        DataLine.Info info  = new DataLine.Info(SourceDataLine.class, format);
+	        try {
+	            line = (SourceDataLine) AudioSystem.getLine(info);
+	            line.open(format);
+	        }
+	        catch (Exception e) {
+	            e.printStackTrace();
+	            return;
+	        }
+	        line.start();
+	        line.write(data, 0, bytes);
+	        line.drain();
+	        line.close();
+	    }
+
+	    // save to a wav file
+	    public void save(File file) {
+	       
+	        AudioFileFormat.Type type = AudioFileFormat.Type.WAVE;
+	        try {
+	            ByteArrayInputStream bais = new ByteArrayInputStream(data);
+	            BufferedInputStream  bis  = new BufferedInputStream(bais);
+	            AudioInputStream ais = new AudioInputStream(bis, format, (long) frames);
+	            AudioSystem.write(ais, type, file);
+	        }
+	        catch (Exception e) {
+	            e.printStackTrace();
+	            return;
+	        }
+	    }
+
+
+	    // return duration
+	    public double duration() {
+	        return seconds;
+	    }
+
+	    // return string representation
+	    public String toString() {
+	        return "" + format;
+	    }
+
+	}
+
+	    
+	    	
+	  
+	    public void interpretImage(BufferedImage img, float timelength){
+	    	long starttime= System.currentTimeMillis();
+	    	int width = img.getWidth();
+	    	int height = img.getHeight();
+	    	float[] samples = new float[(int) (timelength * 44100) ];
+	    	for(int i = 0; i<samples.length; i++){
+	    		samples[i]=0;
+	    	}
+	    	float[] fft = new float[fftsize];
+	    	int samplesPerPixel = samples.length / width ;
+	    	Random rand = new Random();
+	    	for (int s=0; s<width - sliceWidth; s+=pixelsamplesize){
+	    		//each slice
+	    		
+	    		for (int y = 0; y<height; y+=pixelsamplesize){
+	    			int red =0;
+	    			int blue = 0;
+	    			int green = 0;
+	    			for (int x = s; x<s+sliceWidth; x++){
+	    				Color RGB = new Color(img.getRGB(x, y));
+	    				
+	    				red+= RGB.getRed();
+	    				green+=RGB.getGreen();
+	    				blue+= RGB.getBlue();
+	    			}
+	    			
+	    			red = red/sliceWidth;
+	    			blue = blue/sliceWidth;
+	    			green = green/sliceWidth;
+	    			
+	    			float[] hsv = new float[3];
+	    			Color.RGBtoHSB((int)red, (int)green, (int)blue, hsv);
+	    			float basefreq = (24f*(float)y)/height;
+	    			
+	    			
+	    			basefreq+= (12*hsv[0]);
+	    			basefreq -= 10;
+	 
+	    			fft[0] =  220f *(float) Math.pow(2f, (float)((int)basefreq)/12f);
+	    			for (int i =1; i<fft.length; i++){
+	    				fft[i] = fft[i-1] * hsv[0];
+	    			}
+	    		//	if(hsv[2] > 0)System.out.printf("%f, %f, %f\n",fft[0], hsv[1], hsv[2]);
+
+	    			float amp = hsv[2];
+	    			float noiselevel = 1 - hsv[1];
+	    			if (hsv[2]> 0.01){
+	    			for (int i = s*samplesPerPixel ; i< Math.min((s+pixelsamplesize)*samplesPerPixel , samples.length) ;i++){
+	    			
+	    				
+	    				for (int j = 0; j< fftsize; j++){
+	    					samples[i]+=(amp*hsv[1])/(j+1)*sin( fft[j]* i) *rand.nextFloat();
+	    				}
+	    				samples[i] +=amp * noiselevel * 2* (rand.nextFloat()-.5);
+	    			}
+	    			}
+	    			
+	    		}
+	    		
+	    	}
+	    	float maxamp  = Math.abs(samples[0]);
+	    	for (int i  = 0; i< samples.length; i++){
+	    		maxamp = Math.max(Math.abs(samples[i]) ,maxamp  );    			
+	    	}
+	    	float ampmult  = 30f / maxamp;
+	    	byte[] bytes = new byte[samples.length];
+	    	for (int i  = 0; i< samples.length; i++){
+	    		bytes[i] = (byte) (samples[i] * ampmult);
+	    	}
+	    	
+	    	this.currentsound = new Sound(bytes);
+	    	System.out.println(.001 *  (System.currentTimeMillis()-starttime ));
+	    	
+	    }
+	    public void playSound(){
+	    	
+	    	if( currentsound!=null){
+	    		currentsound.play();
+	    	}
+	    	System.out.println("played");
+	    	
+	    }
+	    public void saveFile(File file){
+	    	currentsound.save(file);
+	    }
+	   
+}
+
